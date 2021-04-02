2912ea5b909dddb7c944481d201672266816a172
LenaBlazhnova <lenasudeki@list.ru>
2021-04-02 11:50:40 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
 import React from "react";
 import {Route, BrowserRouter, NavLink} from "react-router-dom";
+import {Menu} from "./components/Menu";
+import {PostList} from "./components/PostList";
+import {Post} from "./components/Post";
 
-function Menu(){
-    return(
-        <nav className="nav">
-            <NavLink className="nav-link active" aria-current="page" to="/">Главная</NavLink>
-            <NavLink className="nav-link" to="/about">О нас</NavLink>
-            <NavLink className="nav-link" to="/contact-us">Контакты</NavLink>
-        </nav>
-    )
-}
-function MainPage(){
-    return(
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="https://i.pinimg.com/originals/41/80/92/418092dcae99c77d214d66595f67541f.jpg" className="d-block w-100" alt="cherry" style={{height:550}}/>
-                </div>
-                <div className="carousel-item">
-                    <img src="https://i.ytimg.com/vi/ctAEWKgCSZA/maxresdefault.jpg" className="d-block w-100" alt="fruits" style={{height:550}}/>
-                </div>
-                <div className="carousel-item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYgZ3fZ-kU50uaX0TWISGXfDODm9pAB-dbww&usqp=CAU" className="d-block w-100" alt="..." style={{height:550}}/>
-                </div>
-            </div>
-        </div>
-    )
-}
+
+
 function AboutUs(){
     return(
         <div className="container my-5">
@@ -53,12 +32,13 @@ function ContactUs(){
 }
 function App() {
     return (
-        <div className="App">
+        <div className="container">
             <BrowserRouter>
                 <Menu/>
-                <Route exact path="/" render={()=><MainPage/>} />
+                <Route exact path="/" render={()=><PostList/>} />
                 <Route path="/about" render={()=><AboutUs/>}/>
                 <Route path="/contact-us" render={()=><ContactUs/>}/>
+                <Route path="/post" render={()=><Post/>}/>
             </BrowserRouter>
         </div>  );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export class Post extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            author: '',
+            text: ''
+        }
+    }
+    componentDidMount() {
+        console.log("Компонет Post отрисован");
+
+        fetch("http://v90355zw.beget.tech/php12/getPosts.php"
+        )
+            .then(response=>response.json())
+            .then(result=>{
+                console.log(result);
+                this.setState({
+                    title: result[0].title,
+                    author: result[0].author,
+                    text: result[0].text
+                });
+            })
+    }
+
+    render() { // Сначала вызывается рендер
+        return <div>
+            <div>
+                <p>Автор: {this.state.author}</p>
+                <p>Статья: {this.state.title}</p>
+            </div>
+            <div><p>{this.state.text}</p></div>
+        </div>
+    }
+}
